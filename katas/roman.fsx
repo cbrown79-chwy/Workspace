e3d463764efb7e0aa5090ac2a3673db5991af63c
Christopher Brown <cmbrown1598@gmail.com>
2017-06-20 14:03:17 -0700

    simpler

--- katas/roman.fsx
+++ katas/roman.fsx
@@ -1,23 +1,19 @@
 let convert intValue =  
-    let swap (stringToFind : string) swapString (inString : string) = 
-        inString.Replace(stringToFind, swapString)
     let rec loop acc n = 
+        let q t v = loop (acc + t) (n - v)
         match n with 
-        | a when a >= 1000 -> loop (acc + "M") (n - 1000)
-        | a when a >= 500 -> loop (acc + "D") (n - 500)
-        | a when a >= 100 -> loop (acc + "C") (n - 100)
-        | a when a >= 50 -> loop (acc + "L") (n - 50)
-        | a when a >= 10 -> loop (acc + "X") (n - 10)
-        | a when a >= 5 -> loop (acc + "V") (n - 5)
-        | a when a >= 1 -> loop (acc + "I") (n - 1)
+        | a when a >= 1000 -> q "M" 1000
+        | a when a >= 900 -> q "CM" 900
+        | a when a >= 500 -> q "D" 500
+        | a when a >= 400 -> q "CD" 400
+        | a when a >= 100 -> q "C" 100
+        | a when a >= 90 -> q "XC" 90
+        | a when a >= 50 -> q "L" 50
+        | a when a >= 40 -> q "XL" 40
+        | a when a >= 10 -> q "X" 10
+        | a when a >= 9 -> q "IX" 9
+        | a when a >= 5 -> q "V" 5
+        | a when a >= 4 -> q "IV" 4
+        | a when a >= 1 -> q "I" 1
         | _ -> acc + ""
     loop "" intValue
-        |> swap "DCCCC" "CM"
-        |> swap "CCCC" "CD"
-        |> swap "LXXXX" "XC"
-        |> swap "XXXX" "XL"
-        |> swap "VIIII" "IX"
-        |> swap "IIII" "IV"
-    
-
-
