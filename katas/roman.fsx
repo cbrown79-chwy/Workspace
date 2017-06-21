642b57b279166958b481956b70abfd4478bdacf7
Christopher Brown <cmbrown1598@gmail.com>
2017-06-21 10:13:39 -0700

    Final changes for readability.

--- katas/roman.fsx
+++ katas/roman.fsx
@@ -1,11 +1,10 @@
 let convert iVal =  
-    let vals = [(1000, "M");(900, "CM");(500, "D");
-                (400, "CD");(100, "C");(90, "XC");(50, "L");
-                (40, "XL");(10, "X");(9,"IX");(5,"V");(4,"IV");(1, "I")]
+    let vals = [(1000, "M");(900, "CM");(500, "D");(400, "CD");
+                (100, "C");(90, "XC");(50, "L");(40, "XL");
+                (10, "X");(9,"IX");(5,"V");(4,"IV");(1, "I")]
     let rec loop acc n list = 
-        let f t v l = loop (acc + t) (n - v) l
         match list with 
-            | (i, s)::xs when n >= i -> f s i list
-            | x::xs -> f "" 0 xs
             | [] -> acc + ""
+            | (i, s)::xs when n >= i -> loop (acc + s) (n - i) list
+            | x::xs -> loop acc n xs
     loop "" iVal vals
