480c0e05dc1852a8c2cff8d4464e2fdfeda523e0
Christopher Brown <cmbrown1598@gmail.com>
2017-07-06 09:18:47 -0700

    I'm still less than happy with the implementation, (still seems wordy), but I like it better than it was. My C# isn't showing as much here.

--- katas/diamond.fsx
+++ katas/diamond.fsx
@@ -1,15 +1,17 @@
 open System
 
 let diamond char = 
-    let space i = new string (' ', i)
     let availableChars = [ 'A' .. char ]
     let location = List.length availableChars
+    let gridWidth = (location * 2 - 1)
+    let pad i = new string (' ', i)
+    let makeTuple ch index = (string ch, index)
+    let en = makeTuple char 0 
     let locCount i = location - (i + 1)
-    let start = availableChars |> List.take (location - 1) |> List.mapi (fun i c -> c, locCount i)
-    let en = (char,0) 
-    let all = start @ (en :: List.rev start)
+    let start = availableChars |> List.mapi (fun i c -> makeTuple c (locCount i))
+    let all = start @ (List.rev start)
     let m = all |> List.map (fun (c, i) -> 
-                       match (location * 2 - 1) - (i * 2 + 1) with 
-                       | 0 -> space i + c.ToString() + space i
-                       | a -> space i + c.ToString() + space (a - 1) + c.ToString() + space i)
+                       match gridWidth - (i * 2 + 1) with 
+                       | 0 -> pad i + c + pad i
+                       | a -> pad i + c + pad (a - 1) + c + pad i)
     Environment.NewLine :: m |> List.reduce (fun x y -> sprintf "%s%s%s" x Environment.NewLine y)
