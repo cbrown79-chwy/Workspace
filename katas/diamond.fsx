53d429ea307a0a34cd50bb986ce831fe08b71493
Christopher Brown <cmbrown1598@gmail.com>
2017-07-10 16:47:06 -0700

    Commit-o-rama

--- katas/diamond.fsx
+++ katas/diamond.fsx
@@ -8,8 +8,9 @@ let diamond char =
     let pad i = new string (' ', i)
     let init = chars |> List.mapi (fun i c -> (string c), (padCount i))
     let all = init @ (List.tail (List.rev init))
-    all |> List.map (fun (str, padCount) -> 
-                       match gridWidth - (padCount * 2 + 1) with 
-                       | 0 -> pad padCount + str + pad padCount
-                       | a -> pad padCount + str + pad (a - 1) + str + pad padCount)                   
+    let makeLine (str,padCount) = 
+        match gridWidth - (padCount * 2 + 1) with
+       | 0 -> pad padCount + str + pad padCount
+       | a -> pad padCount + str + pad (a - 1) + str + pad padCount
+    all |> List.map makeLine
         |> List.reduce (fun x y -> sprintf "%s%s%s" x Environment.NewLine y)
