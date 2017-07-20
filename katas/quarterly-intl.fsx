e95f0bb643f9b7d264e265fbf331f39d626de55e
Christopher Brown <cmbrown1598@gmail.com>
2017-07-20 16:20:54 -0700

    Missing commit.

--- katas/quarterly-intl.fsx
+++ katas/quarterly-intl.fsx
@@ -25,10 +25,17 @@ let modelInfo (n:QuarterlyIntlCharx.Row) = {
                Weights = [n.B_Wt1;n.B_Wt2;n.B_Wt3;n.B_Wt4;n.B_Wt5;n.B_Wt6;n.B_Wt7;n.B_Wt8;n.B_Wt9;n.B_Wt10;n.B_Wt11;n.B_Wt12;n.B_Wt13;(decimal n.B_Wt14);(decimal n.B_Wt15);n.B_Wt16;n.B_Wt17;n.B_Wt18;n.B_Wt19;n.B_Wt20;n.B_Wt21;n.B_Wt22;n.B_Wt23;n.B_Wt24;n.B_Wt25;n.B_Wt26;n.B_Wt27;n.B_Wt28]
             }
 
-let getModelsWithMultipleValuesPerShortname() = 
+let benchmarksAreUnique() = 
     quarterlyData.Rows 
         |> Seq.map modelInfo 
         |> Seq.distinct 
         |> Seq.groupBy (fun item -> item.ShortName)
         |> Seq.filter (fun (key, s) -> Seq.length s > 1)
         |> List.ofSeq
+    
+let weightsSumTo100() = 
+    quarterlyData.Rows 
+        |> Seq.map modelInfo 
+        |> Seq.distinct 
+        |> Seq.filter (fun item -> abs (List.sum item.Weights - 100.0M) > 2.0M)
+        |> List.ofSeq    
