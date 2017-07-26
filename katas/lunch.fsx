3da3693605ed95de9ef0f1fb401c8a60bc7dc777
Christopher Brown <cmbrown1598@gmail.com>
2017-07-26 12:30:14 -0700

    Moving merge, as it has no other consumers.

--- katas/lunch.fsx
+++ katas/lunch.fsx
@@ -7,8 +7,6 @@ type Message =
 
 type Lunch = { Name : string; Votes : int }
 
-let merge lunch1 lunch2 = { lunch1 with Votes = lunch1.Votes + lunch2.Votes}
-
 let voteMachine = MailboxProcessor<Message>.Start(fun inbox ->
         let rec loop (state: Lunch list) = async {
             let! msg = inbox.Receive()
@@ -20,7 +18,8 @@ let voteMachine = MailboxProcessor<Message>.Start(fun inbox ->
             | Downvote s | Upvote s -> 
                 let newLunch = match msg with 
                                     | Downvote _ -> { Name = s; Votes = -2};
-                                    | _ -> { Name = s; Votes = 1}                                    
+                                    | _ -> { Name = s; Votes = 1}
+                let merge lunch1 lunch2 = { lunch1 with Votes = lunch1.Votes + lunch2.Votes}
                 let newState = newLunch::state 
                                 |> List.groupBy (fun n -> n.Name)
                                 |> List.map (fun (s, l) -> List.reduce merge l)
@@ -29,7 +28,9 @@ let voteMachine = MailboxProcessor<Message>.Start(fun inbox ->
             | DownAndDestroy s ->
                 let direction name = if name = s then -2 
                                      else 1
-                let newState = state |> List.map (fun n -> { n with Votes = n.Votes + direction n.Name }) |> List.sortByDescending (fun n -> n.Votes)
+                let newState = state 
+                                |> List.map (fun n -> { n with Votes = n.Votes + direction n.Name }) 
+                                |> List.sortByDescending (fun n -> n.Votes)
                 return! loop newState
             | Render ->
                 state 
