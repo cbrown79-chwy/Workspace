b4c8d1dd2b43223ac41f49c4c9f46e37e4ffc943
Christopher Brown <cbrown@paraport.com>
2017-12-21 13:51:23 -0800

    cleen

--- katas/yearmonth.fsx
+++ katas/yearmonth.fsx
@@ -1,8 +1,9 @@
 let m = (System.DateTime.Now.Year * 100) + System.DateTime.Now.Month
 
-[|199001 .. m|] |> Array.filter (fun n -> let m = n%100
-                                          match m with 
-                                          | 0  -> false
-                                          | i when i > 12 -> false
-                                          | _ -> true
-                                          )
+[|199001 .. m|] 
+    |> Array.filter (fun n -> 
+                        let m = n%100
+                        match m with 
+                        | 0  -> false
+                        | i when i > 12 -> false
+                        | _ -> true )
